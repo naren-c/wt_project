30b47e4258d141f7ea1528f860d532c4ec555266
P K Navin Shrinivas <karupal2002@gmail.com>
2021-12-07 16:31:09 -0800

    creating new comp

---
+++ src/land.js
@@ -0,0 +1,15 @@
+import React from "react";;
+import "land.css"
+
+
+export default class Land extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            teststate : 0
+        }
+    }
+    render(){
+        return(<div></div>)
+    }
+}
